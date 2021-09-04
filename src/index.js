e8e41dbbad36c1b086a122df0e91d1078695966e
Muhammad Dafi Ramadhan <mdafiramadhan@yahoo.com>
2021-09-04 14:21:29 +0700

    Modul 2 Kelompok 46
    Anggota Kelompok :
    - Muhammad Dafi Ramadhan
    - Elvitro Gumelar Agung

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import CardList from "./components/CardList";
+import Gambar from "./assets/images/Desain.jpg"; 
+import Quotes from "./components/Quotes";
+
+const quotes = "Disaat	payung	teduh	ku	dengar	,	disitu	lah	tugas-tugas terbabat abis";
+
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <CardList name="Dafi" nim="21120118130080" kelompok="46" isNameBold image={Gambar} />
+    <Quotes author="Orang" quotes={quotes} />
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +22,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
